a11d95ff34818dc737b40b13c118c0de36900335
habeeb shaik <habeeb6318@gmail.com>
2021-02-10 16:44:30 +0530

    committed updated classes

--- modules/guestbook/guestbook-api/src/main/java/com/liferay/docs/guestbook/service/GuestbookLocalService.java
+++ modules/guestbook/guestbook-api/src/main/java/com/liferay/docs/guestbook/service/GuestbookLocalService.java
@@ -117,6 +117,10 @@ public interface GuestbookLocalService
 	@Indexable(type = IndexableType.DELETE)
 	public Guestbook deleteGuestbook(long guestbookId) throws PortalException;
 
+	public Guestbook deleteGuestbook(
+			long guestbookId, ServiceContext serviceContext)
+		throws PortalException, SystemException;
+
 	/**
 	 * @throws PortalException
 	 */
@@ -323,5 +327,10 @@ public interface GuestbookLocalService
 	 */
 	@Indexable(type = IndexableType.REINDEX)
 	public Guestbook updateGuestbook(Guestbook guestbook);
+
+	public Guestbook updateGuestbook(
+			long userId, long guestbookId, String name,
+			ServiceContext serviceContext)
+		throws PortalException, SystemException;
 
 }
--- modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/impl/GuestbookLocalServiceImpl.java
+++ modules/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/impl/GuestbookLocalServiceImpl.java
@@ -16,9 +16,12 @@ package com.liferay.docs.guestbook.service.impl;
 
 import com.liferay.docs.guestbook.exception.GuestbookNameException;
 import com.liferay.docs.guestbook.model.Guestbook;
+import com.liferay.docs.guestbook.model.GuestbookEntry;
+import com.liferay.docs.guestbook.service.GuestbookEntryLocalService;
 import com.liferay.docs.guestbook.service.base.GuestbookLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.OrderByComparator;
@@ -28,6 +31,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * The implementation of the guestbook local service.
@@ -97,4 +101,47 @@ public class GuestbookLocalServiceImpl extends GuestbookLocalServiceBaseImpl {
 			throw new GuestbookNameException();
 		}
 	}
+	
+	public Guestbook updateGuestbook(long userId, long guestbookId,
+		    String name, ServiceContext serviceContext) throws PortalException,
+		                SystemException {
+
+		        Date now = new Date();
+
+		        validate(name);
+
+		        Guestbook guestbook = getGuestbook(guestbookId);
+
+		        User user = userLocalService.getUser(userId);
+
+		        guestbook.setUserId(userId);
+		        guestbook.setUserName(user.getFullName());
+		        guestbook.setModifiedDate(serviceContext.getModifiedDate(now));
+		        guestbook.setName(name);
+		        guestbook.setExpandoBridgeAttributes(serviceContext);
+
+		        guestbookPersistence.update(guestbook);
+
+		        return guestbook;
+		}
+	public Guestbook deleteGuestbook(long guestbookId,
+            ServiceContext serviceContext) throws PortalException,
+            SystemException {
+
+    Guestbook guestbook = getGuestbook(guestbookId);
+
+    List<GuestbookEntry> entries = _guestbookEntryLocalService.getGuestbookEntries(
+                    serviceContext.getScopeGroupId(), guestbookId);
+
+    for (GuestbookEntry entry : entries) {
+            _guestbookEntryLocalService.deleteGuestbookEntry(entry.getEntryId());
+    }
+
+    guestbook = deleteGuestbook(guestbook);
+
+    return guestbook;
+    
+}
+	@Reference
+	private GuestbookEntryLocalService _guestbookEntryLocalService;
 }
